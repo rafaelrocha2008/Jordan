e491d11f5753be05f850fadb3c3f170d13910c4c
rafaelrocha2008 <116768826+rafaelrocha2008@users.noreply.github.com>
2023-02-14 22:09:37 -0300

    Add files via upload

---
+++ server.php
@@ -0,0 +1,9 @@
+<?php
+	  $username = 'root';
+      $password ='';
+    try{
+      $pdo = new PDO('"mysql_host=localhost;dbname=Jordan', $username, $password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      $query = 
+    }
+?>
