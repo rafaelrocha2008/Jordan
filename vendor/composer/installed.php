e7e247565de0399829b3cc55df2ed711f3658661
rafaelrocha2008 <rafael.santos.rocha@protonmail.com>
2023-02-15 00:28:16 -0300

    cscscacc

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'ad98abe858ba6506ee654cf03c603e7d79d0f4f7',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4964b32d0337791e210506952fe948468b397cbf',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,20 +11,11 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'ad98abe858ba6506ee654cf03c603e7d79d0f4f7',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4964b32d0337791e210506952fe948468b397cbf',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
-            'aliases' => array(),
-            'dev_requirement' => false,
-        ),
-        'alrik11es/cowsayphp' => array(
-            'pretty_version' => '1.2.0',
-            'version' => '1.2.0.0',
-            'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
-            'type' => 'library',
-            'install_path' => __DIR__ . '/../alrik11es/cowsayphp',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
--- web/server.php
+++
@@ -1,9 +0,0 @@
-<?php
-	  $username = 'root';
-      $password ='';
-    try{
-      $pdo = new PDO('"mysql_host=localhost;dbname=Jordan', $username, $password);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-      $query = 
-    }
-?>
