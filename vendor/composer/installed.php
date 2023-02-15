f839f0340e1d1ea05d0ad60d5e73e321b6e523aa
rafaelrocha2008 <rafael.santos.rocha@protonmail.com>
2023-02-14 23:22:46 -0300

    Demo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ce8dd4e3ff5c4769fa2fb2535bf00619658e50f9',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ce8dd4e3ff5c4769fa2fb2535bf00619658e50f9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'alrik11es/cowsayphp' => array(
+            'pretty_version' => '1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '080dc10c8f5420fef35163569293601f17124eeb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../alrik11es/cowsayphp',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
